435466f46cdeef7382d8cae4d6982c9f0bb5c886
Lollo <lorenzo.lefosse@gmail.com>
2023-10-17 11:40:49 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ import os
 from chromadb.config import Settings
 
 # https://python.langchain.com/en/latest/modules/indexes/document_loaders/examples/excel.html?highlight=xlsx#microsoft-excel
-from langchain.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader
+from langchain.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader, JSONLoader
 from langchain.document_loaders import UnstructuredFileLoader
 
 
