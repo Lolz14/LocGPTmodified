6628ad6316237ef087db84805471950e9f137d2d
Lollo <lorenzo.lefosse@gmail.com>
2023-11-22 12:30:41 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -57,12 +57,12 @@ DOCUMENT_MAP = {
 }
 
 # Default Instructor Model
-#EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"  # Uses 1.5 GB of VRAM (High Accuracy with lower VRAM usage)
+EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"  # Uses 1.5 GB of VRAM (High Accuracy with lower VRAM usage)
 
 ####
 #### OTHER EMBEDDING MODEL OPTIONS
 ####
-EMBEDDING_MODEL_NAME = "jinaai/jina-embeddings-v2-base-en"
+# EMBEDDING_MODEL_NAME = "jinaai/jina-embeddings-v2-base-en"
 # EMBEDDING_MODEL_NAME = "hkunlp/instructor-xl" # Uses 5 GB of VRAM (Most Accurate of all models)
 # EMBEDDING_MODEL_NAME = "intfloat/e5-large-v2" # Uses 1.5 GB of VRAM (A little less accurate than instructor-large)
 # EMBEDDING_MODEL_NAME = "intfloat/e5-base-v2" # Uses 0.5 GB of VRAM (A good model for lower VRAM GPUs)
